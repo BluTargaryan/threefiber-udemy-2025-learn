a61929293c9cc84fbf5967ba190d486b007b7fe7
BluTargaryan <opeyemiyolo@gmail.com>
2025-04-25 23:12:18 +0100

    lesson day 8 pt 2

--- src/app/components/BoxCanvasV2.tsx
+++ src/app/components/BoxCanvasV2.tsx
@@ -1,15 +1,16 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useLoader } from '@react-three/fiber'
 import PolyhedronV2 from './PolyhedronV2'
 import * as THREE from 'three'
 import { Stats, OrbitControls, Polyhedron } from '@react-three/drei'
 import Lights from './Lights'
 import Floor from './Floor'
 export default function App() {
+  const texture = useLoader(THREE.TextureLoader, '/grid.png')
   return (
     <Canvas camera={{ position: [-1, 4, 2.5] }} shadows>
       {/* <directionalLight position={[1, 1, 1]} /> */}
       <Lights />
-      <PolyhedronV2
+      {/* <PolyhedronV2
         name="meshBasicMaterial"
         position={[-3, 1, 0]}
         material={new THREE.MeshBasicMaterial()}
@@ -28,6 +29,30 @@ export default function App() {
         name="meshStandardMaterial"
         position={[3, 1, 0]}
         material={new THREE.MeshStandardMaterial()}
+      /> */}
+       <PolyhedronV2
+        name="meshBasicMaterialTextured"
+        position={[3, 1, 0]}
+        material={new THREE.MeshBasicMaterial({ map: texture })}
+      />
+      <PolyhedronV2
+        name="meshPhongMaterialTextured"
+        position={[1, 1, 0]}
+        material={new THREE.MeshPhongMaterial({
+          map: texture,
+        })}
+      />
+      <PolyhedronV2
+        name="meshNormalMaterialTextured"
+        position={[-1, 1, 0]}
+        material={new THREE.MeshNormalMaterial()}
+      />
+      <PolyhedronV2
+        name="meshStandardMaterial"
+        position={[-3, 1, 0]}
+        material={new THREE.MeshStandardMaterial({
+          map: texture,
+        })}
       />
       <Floor />
       <OrbitControls target-y={1} />
